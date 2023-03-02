d12fe95cab98a03c074cb76011db009122205891
abhishekdey <abhishekdey@users.noreply.github.com>
2023-03-02 10:35:50 +0530

    groupcheck

--- src/app/lp-section/lp-section.component.ts
+++ src/app/lp-section/lp-section.component.ts
@@ -29,34 +29,34 @@ export class LpSectionComponent implements OnInit {
 
   ngOnInit() {
     this.dataList = [
-      { policyNumber: 'NGXE1006-2022R7111', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R71', status: '33Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc1', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R71h1', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r', status: '33Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc2', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r3', status: '33Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc3', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r3', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r36', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r34', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r3', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r333', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711rff', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R711r33', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
-      { policyNumber: 'NGXE1006-2022R7j99', status: 'Collecting Policy Prerequisites1', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' }];
+      { policyNumber: '0', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '1', status: '33Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc1', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '2', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '3', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '4', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '5', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '6', status: '33Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc2', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '7', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '8', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '9', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '10', status: '33Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc3', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '11', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '12', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '13', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '14', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '15', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '16', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '17', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' },
+      { policyNumber: '18', status: 'Collecting Policy Prerequisites', groupName: 'Earnhardt Manufacturing Inc', notes: 'SB 7/19 licencing resort', effectiveDate: new Date('03-1-1986'), tpa: 'Health Plans Inc', underwriter: 'NG', producer: 'Health Plans Inc.' }];
     //this.dataList = this.dataList.map(connect1);
     // function connect1(dateCurrent, index) {
     //   return { ...dateCurrent, parentIndex: index, childIndex: null };
     // }
     this.currentDataList = [...this.dataList];
-    this.currentDataList.map((data, index) => { data.parentIndex = index, data.childId = null, data.oldId = index, data });
+    this.currentDataList.map((data, index) => { data.parentIndex = index, data.childId = null, data.oldId = index, data.isGroup = false, data });
     this.totalItem = this.currentDataList.length;
     this.fixes = Object.keys(this.currentDataList[0]);
-    this.fixes =this.fixes.filter((data) => ['parentIndex', 'childId', 'oldId'].indexOf(data) < 0);
+    this.fixes =this.fixes.filter((data) => ['parentIndex', 'childId', 'oldId', 'isGroup'].indexOf(data) < 0);
     this.checked = this.fixes.map(() => { const checked = false });
   }
 
@@ -74,10 +74,14 @@ export class LpSectionComponent implements OnInit {
   applyMerge() {
     this.selectedRow = this.currentDataList[this.parentIndex];
     this.resetChildGroup(this.parentIndex);
+    //console.log(this.currentDataList);
     const X1 = this.currentDataList.filter((data, index) => index !== this.parentIndex && this.groupCheck(data));
+    
+    this.selectedRow['isGroup'] = !!X1.length;
     X1.map((data) => { data.childId = this.parentIndex,  data.parentIndex = null; });
-    this.apply(null);
-    console.log(this.currentDataList);
+    this.apply(this.parentIndex);
+    //console.log(X1);
+    //console.log(this.currentDataList, this.checked);
   }
 
   resetChildGroup(parentIndex) {
@@ -97,13 +101,23 @@ export class LpSectionComponent implements OnInit {
     //a.childIndex = null;
     a.parentIndex = a.oldId;
     //a.parentIndex = a.oldId;
-    console.log(this.currentDataList);
+    //console.log(this.currentDataList);
   }
 
   groupCheck(data) {
-    const filterList = this.checked.filter((dat, index) => data.childId === null && data[this.fixes[index]] === this.selectedRow[this.fixes[index]] && dat);
+    const filterList = this.checked.filter((dat, index) => dat && data.childId === null && !data.isGroup  && data.parentIndex !==null && data[this.fixes[index]] === this.selectedRow[this.fixes[index]]);
+    //console.log(filterList);
     return filterList.length === this.checked.filter((data) => data).length && filterList.length > 0;
   }
+
+  // isGroupTest(parentIndex) {
+  //   if(parentIndex) {
+  //     const a = [...this.currentDataList];
+  //     const filterList = a.filter((dat, index) => dat.childId !== parentIndex && dat.parentIndex !==null);
+  //     console.log(filterList,parentIndex);
+  //     return filterList.length;
+  //   }
+  // }
 
   selected(extra, id) {
     return this.checked[id] === extra;
